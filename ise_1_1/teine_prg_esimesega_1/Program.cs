954e3d66f163935a2b0dde5ea5c71b3bd0b9d4b3
eerotpt <eero.sakson@tptlive.ee>
2017-10-25 17:03:41 +0200

    entry_2_mõlemad
    kaks koos

--- ise_1_1/teine_prg_esimesega_1/Program.cs
+++ ise_1_1/teine_prg_esimesega_1/Program.cs
@@ -4,22 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ise_1_1
+namespace teine_prg_esimesega_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //algus
-            Console.WriteLine("Tere");
+            //teine_esimesega
+            Console.WriteLine("33");
             Console.WriteLine("1");
-            Console.WriteLine("");
+            Console.WriteLine("Hei!");
 
             Console.WriteLine("2");
-            Console.WriteLine("Tere");
+            Console.WriteLine("33");
 
             Console.ReadLine();
-
         }
     }
 }
