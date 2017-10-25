d99f10b2ecea85a49230f9e329e471162fa654ce
eerotpt <eero.sakson@tptlive.ee>
2017-10-25 17:26:02 +0200

    neljas_summary
    neljas_description

---
+++ ise_1_1/soft_4_so_to_say/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soft_4_so_to_say
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////Neljas entry
+        }
+    }
+}
